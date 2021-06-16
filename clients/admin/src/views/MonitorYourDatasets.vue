6c73896ee0c7d96d3d8366f06c5ee72b9adb8909
Sharanya Manivasagam <sharanym@uio.no>
2021-06-16 13:44:53 +0200

    fixes for dataset store

--- clients/admin/src/views/MonitorYourDatasets.vue
+++ clients/admin/src/views/MonitorYourDatasets.vue
@@ -8,13 +8,11 @@
           id="wardtable-filter-text-box"
           placeholder="Filter..."
           class="border-2 absolute left-0"
-          v-model="searchField"
-          @input="onWardTableSearchTextChanged"
         />
         <div>{{ 'MonitorYourDatasets' }}</div>
       </div>
 
-      <AgGridVue
+      <!-- <AgGridVue
         style="width: 100%; height: 50vh"
         class="ag-theme-alpine"
         :gridOptions="gridOptions"
@@ -23,61 +21,21 @@
         :defaultColDef="{ editable: true, sortable: true }"
         @cellClicked="cellClicked"
       >
-      </AgGridVue>
+      </AgGridVue> -->
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { AgGridVue } from 'ag-grid-vue3'
-import { GridOptions, CellEvent } from '@ag-grid-community/all-modules'
+import { defineComponent } from 'vue'
+
+// import { GridOptions, CellEvent } from '@ag-grid-community/all-modules'
 
 export default defineComponent({
   name: 'MonitorYourDatasets',
-  components: {
-    AgGridVue,
-  },
+  components: {},
   setup() {
-    const gridOptions: GridOptions = {
-      suppressScrollOnNewData: true,
-    }
-
-    const searchField = ref('')
-    const selectedRow = ref()
-
-    // Translate the headerNames
-    const columnDefs = [
-      { headerName: 'Hello' },
-      { headerName: 'Hello' },
-      { headerName: 'Hello' },
-    ]
-
-    const rowData = ['1', '2', '3']
-
-    // Filter all columns based on the text
-    // If one of the columns contain the text, the table will show them
-    const onWardTableSearchTextChanged = () => {
-      gridOptions.api?.setQuickFilter(searchField.value)
-    }
-
-    // Handle Table Events
-    const cellClicked = (event: CellEvent) => {
-      selectedRow.value = event
-    }
-
-    return {
-      columnDefs,
-      rowData,
-      gridOptions,
-      searchField,
-
-      // Table events
-      cellClicked,
-
-      // View
-      onWardTableSearchTextChanged,
-    }
+    return {}
   },
 })
 </script>
