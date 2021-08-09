8620a0f015f66a9dc3e1f73f8d9ea7ee90a0fe89
richarne <richarne@uio.no>
2021-08-09 23:10:13 +0200

    Add check for IAM creds

--- server/services/storage.js
+++ server/services/storage.js
@@ -5,6 +5,8 @@ const {
   ListObjectsCommand,
 } = require('@aws-sdk/client-s3')
 
+const { STSClient, GetCallerIdentityCommand } = require('@aws-sdk/client-sts')
+
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 const fs = require('fs')
 const crypto = require("crypto")
@@ -26,13 +28,16 @@ const s3Configuration = {
   }, */
   region: process.env.AWS_BUCKET_REGION,
   forcePathStyle: true,
+  tls: true,
   endpoint: {
+    protocol: 'https',
     hostname: process.env.AWS_BUCKET_ENDPOINT,
+    port: 443
   },
 };
 
 const s3 = new S3Client(s3Configuration)
-// console.log(s3.config.credentials())
+const sts = new STSClient(s3Configuration);
 
 async function listBucketItems() {
   const bucketParams = { Bucket: process.env.AWS_BUCKET_NAME };
@@ -44,6 +49,17 @@ async function listBucketItems() {
   }
 }
 listBucketItems()
+
+async function getIdentity() {
+  const command = new GetCallerIdentityCommand({});
+  try {
+    const data = await sts.send(command);
+    console.log("Success", data);
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+getIdentity()
 
 /**
  * Upload a file to S3
